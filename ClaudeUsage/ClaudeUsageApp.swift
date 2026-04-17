0535f14c609f36ad65d76dfe169269bd9be32c04
Takuya Otani <takuya.otani@serenebach.net>
2026-04-18 10:32:23 +1200

    Initial Commit

---
+++ ClaudeUsage/ClaudeUsageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeUsageApp.swift
+//  ClaudeUsage
+//
+//  Created by Takuya Otani on 2026/04/18.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeUsageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
